10302ca5be9608a2445d00d593e2c25cb57c3819
bruna.gil <bruna.gil@BGIL>
2019-05-09 15:48:25 -0300

    create second example for web workers presentation

---
+++ web-workers/example2/worker.js
@@ -0,0 +1,11 @@
+
+let n = 1;
+search: while (true) {
+  n += 1;
+  for (let i = 2; i <= Math.sqrt(n); i += 1)
+    if (n % i == 0)
+     continue search;
+  
+     //Encontra o número primo e comunica para o script principal:
+  postMessage(n);
+}
